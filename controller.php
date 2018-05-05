c376ee2e047d13c0c577f46b98e4ba7949c49071
Sebastian Rodriguez <srodriguez@browning.edu>
2018-05-04 20:48:35 -0400

    Initial

---
+++ controller.php
@@ -0,0 +1,40 @@
+<?php
+	$setup = '
+		<meta charset="utf-8">
+		<meta name="description" content="The Tardigrade Group">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="assets/custom.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.min.css.map">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	';
+
+	$footer = '
+		<footer class="footer">
+		  <div class="container">
+		    <div class="content has-text-centered">
+		      <p>© 2017 <b>The Tardigrade Group</b>.</p>
+		      <h1 class="title">
+					<a href="#" class="fb">
+						<i class="fa fa-facebook"></i>
+					</a>&nbsp;
+					<a href="#" class="tw">
+						<i class="fa fa-twitter"></i>
+					</a>&nbsp;
+					<a href="https://www.instagram.com/tardigradegroup/" class="ig">
+						<i class="fa fa-instagram"></i>
+					</a>
+				</h1>
+		    </div>
+		  </div>
+		</footer>
+		<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script>
+			$(".nav-toggle").click(function() {
+				$(".nav-menu").toggleClass("click");
+				$(this).toggleClass("click");
+			});
+		</script>
+	';
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+  	<title>Home</title>
+    <?php include "controller.php"; ?>
+    <?php echo $css;?>
+  </head>
+
+  <body>
+
+    <?php echo $nav;?>
+
+    <div class="wrapper">
+      <div class="header"></div>
+
+      <section>
+        <h1>Main Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet elementum ligula. Suspendisse potenti. Donec ultrices luctus luctus. Curabitur faucibus, mi posuere imperdiet mattis, nisi ipsum ornare magna, in porttitor urna erat eget mi. Etiam eu diam ac ex porta tempus a bibendum libero. Quisque egestas velit turpis. Curabitur tempus consectetur vulputate. Sed vitae cursus dolor, et hendrerit lectus. Morbi ut luctus odio.</p>
+        <p>Integer eu felis turpis. Proin vitae dui metus. Nulla luctus augue id velit pellentesque suscipit. Sed a pretium orci. Vestibulum id magna ex. Nam eget sapien laoreet, sollicitudin ligula eu, rutrum lectus. Proin rhoncus nulla et diam varius, nec convallis nisi placerat. Pellentesque fermentum est odio, at tempor eros laoreet vitae.</p>
+        <p>Quisque malesuada elementum lectus eu malesuada. Aenean fermentum est vel diam dictum volutpat. Phasellus vitae dui risus. Pellentesque pharetra velit ut nisl ullamcorper, id rutrum lectus tincidunt. Nulla egestas laoreet lacinia. Donec hendrerit enim ex, quis rutrum ex commodo a. Fusce a dui lectus. Etiam eros turpis, elementum at augue a, placerat accumsan ligula. Vivamus congue felis ac sem lobortis, sed fringilla dolor blandit.</p>
+        <p>Etiam posuere viverra metus, at luctus lectus congue a. Praesent rhoncus risus vitae turpis tristique rutrum. Curabitur a urna vel tortor finibus sodales vel at ex. Aenean iaculis massa non ex convallis, ac rhoncus leo aliquam. Quisque ut eleifend enim. Nulla laoreet varius dolor nec dictum. Donec et tempus orci, sed ultrices metus. Cras blandit imperdiet eleifend. Donec a faucibus turpis. Mauris tempor, mauris eu faucibus commodo, urna risus feugiat nulla, fermentum varius nisi est eu nibh. Duis euismod porttitor lacinia. Suspendisse mollis sagittis neque, consectetur convallis nisi tristique quis. Donec ornare nulla id nibh sollicitudin, et imperdiet enim elementum.</p>
+        <p>In condimentum ultrices nisi eu cursus. Nullam orci nulla, volutpat non condimentum in, tristique id turpis. Mauris rhoncus congue massa sed ornare. Quisque finibus non sapien ultricies interdum. Nulla facilisi. Praesent arcu mi, interdum nec pulvinar vel, feugiat ut leo. Nulla vulputate, felis sed blandit fringilla, justo nulla laoreet arcu, sollicitudin scelerisque libero neque nec nunc. Quisque pharetra risus risus, rutrum maximus felis egestas vitae.</p>
+      </section>
+
+    </div>
+
+    <?php echo $navjs;?>
+  </body>
+</html>
